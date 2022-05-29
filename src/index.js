dfabff9917c7ab7ee286110abee4ea3e99235c55
Alexey Anisimov <alexkwyk@gmail.com>
2022-05-30 01:32:01 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 import * as fs from 'node:fs';
 import _ from 'lodash'
-const filepath1 = './assets/file1.json';
-const filepath2 = './assets/file2.json';
-const file1 = JSON.parse(fs.readFileSync(filepath1));
-const file2 = JSON.parse(fs.readFileSync(filepath2));
-const firstFileValues = Object.entries(file1);
-const secondFileValues = Object.entries(file2);
-const mergedValues = [...firstFileValues, ...secondFileValues];
-const result = mergedValues.reduce((acc, item) => {
-  const [key, value] = item;
-  const output = item.join(': ');
-  if (file1[key] === value && file2[key] === value) {
-    const output = item.join(': ');
-    if (acc.includes(output)) {
-      return [...acc];
+
+const genDiff = (filepath1, filepath2) => {
+  const file1 = JSON.parse(fs.readFileSync(filepath1));
+  const file2 = JSON.parse(fs.readFileSync(filepath2));
+
+  const mergedValues = [...Object.keys(file1), ...Object.keys(file2)];
+  const uniqueValues = _.uniq(mergedValues);
+
+  const result = uniqueValues.reduce((acc, key) => {
+    const [firstFileOutput, commonOutput, secondFileOutput] = acc;
+    const hasKey1 = Object.hasOwn(file1, key);
+    const hasKey2 = Object.hasOwn(file2, key);
+    const Value1 = file1?.[key];
+    const Value2 = file2?.[key];
+    if (hasKey1 === hasKey2) {
+      if (Value1 === Value2) {
+        commonOutput.push(`${key}: ${Value1}`)
+        return acc;
+      }
+      commonOutput.push(`- ${key}: ${Value1}`);
+      commonOutput.push(`+ ${key}: ${Value2}`);
+      return acc;
     }
-    return [...acc, output];
-  }
-  if (file1[key] === value) {
-    return [...acc, `- ${output}`];
-  }
-  if (file2[key] === value) {
-    return [...acc, `+ ${output}`];
-  }
-}, [])
-.sort((a,b) => {
-  if (a.slice(0,1) === '-') return -1;
-  if (a.slice(0,1) === '+') return 0;
-  return 0;
-});
-console.log(result)
+    if (hasKey1) {
+      firstFileOutput.push(`- ${key}: ${Value1}`);
+      return acc;
+    }
+    if (hasKey2) {
+      secondFileOutput.push(`+ ${key}: ${Value2}`);
+      return acc;
+    }
+  }, [[], [], []]).flat();
+  return `{\n  ${result.join('\n  ')} \n}`;
+};
+
+export default genDiff;
