31ea1d128bfb29a2b37299a888a27ed9c31c207a
Alexey Anisimov <alexkwyk@gmail.com>
2022-06-01 16:07:32 +0300

    add YAML file tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,10 +8,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFilePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const file1 = getFilePath('file1.json');
-const file2 = getFilePath('file2.json');
-const expectedString = '{\n  - proxy: 123.234.53.22\n  - follow: false\n  host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  + verbose: true \n}'
+
+const file1json = getFilePath('file1.json');
+const file2json = getFilePath('file2.json');
+const file1yml = getFilePath('file1.yml');
+const file2yml = getFilePath('file2.yml');
+const expectedString = '{\n  - proxy: 123.234.53.22\n  - follow: false\n  host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  + verbose: true \n}';
 
 test('JSON files',() => {
-  expect(genDiff(file1, file2)).toEqual(expectedString);
+  expect(genDiff(file1json, file2json)).toEqual(expectedString);
+});
+
+test('YAML files',() => {
+  expect(genDiff(file1yml, file2yml)).toEqual(expectedString);
 });
