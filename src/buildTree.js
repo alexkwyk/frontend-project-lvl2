c2e1d0afac2710660c0e971337e9f0118858e470
Alexey Anisimov <alexkwyk@gmail.com>
2022-06-14 22:33:40 +0300

    fix

--- src/buildTree.js
+++ src/buildTree.js
@@ -6,10 +6,10 @@ const buildTree = (data1, data2) => {
   return sortedKeys.map((key) => {
     const value1 = data1?.[key];
     const value2 = data2?.[key];
-    if (_.has(data1, key) && !_.has(data2, key)) {
+    if (!_.has(data2, key)) {
       return { key, type: 'removed', value: value1 };
     }
-    if (_.has(data2, key) && !_.has(data1, key)) {
+    if (!_.has(data1, key)) {
       return { key, type: 'added', value: value2 };
     }
     if (_.isObject(value1) && _.isObject(value2)) {
